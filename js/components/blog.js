d462572a555032fe68c571e5a851a79364b99218
Oana Peres <oana.peres@evozon.com>
2016-03-09 07:28:43 -0800

    minor work on blog component

--- js/components/blog.js
+++ js/components/blog.js
@@ -7,9 +7,22 @@ class MateriableBlog extends React.Component {
 
     render() {
         return (
-    		<Block className={'e-container '}>
-    			
-				</Block>
+        <Block>
+          <Block classes='top-blog e-background-indigo-500'>
+          </Block>
+      		<Block className={'e-container go-top'}>
+            <Block className={'e-row'}>
+              <Block classes={'brick brick-4 border'}>
+                <Text type={'h1'} classes={'e-text-center e-text-white no-margin-bottom'}>Materiable
+                </Text>
+                <Text type={'p'} classes={'e-text-center e-text-light-blue-300 e-right under-right'}>Blog
+                </Text>
+              </Block>
+              <Block classes={'brick brick-8 border'}>
+              </Block>
+            </Block>
+  				</Block>
+        </Block>
 
         );
     }
