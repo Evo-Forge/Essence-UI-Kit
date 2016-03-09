e8753cd09001347b123c214b1f57fcfc09d2b89d
Radu Farcas <f.radu10@yahoo.com>
2016-03-09 17:16:17 +0200

    minor css changes

--- js/components/other.js
+++ js/components/other.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ClassNames from 'classnames';
 
-import { Btn, AppBar, Block, Text, Utils, Menu, List, ListItem} from 'react-essence';
+import { Btn, AppBar, Block, Text, Utils, Menu, List, ListItem, Paper} from 'react-essence';
 
 class MateriableOther extends React.Component {
 
     render() {
         return (
-          <Block className={'e-container '}>
-
-          </Block>		
+          <Block>
+            <Block className={'header-bg e-background-indigo-500'}>
+            </Block>
+            <Block className={'main-bg e-background-grey-100'}>
+              <Block className={'e-container '}>
+                <Paper className={'e-text-indigo-400 e-background-white'}>
+                  <Text type={'h2'} classes={'e-text-center e-display-2'}>
+                    headings
+                  </Text>
+                </Paper>
+              </Block>
+            </Block>
+          </Block>
         );
     }
 }
