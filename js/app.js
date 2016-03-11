9ef0432702910f0f7ee7ed103bbcc6a08f053058
Razvan Matei <razvanmateid@gmail.com>
2016-03-11 12:01:44 +0200

    create about.less & about.js

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ import MateriableHome from './components/home';
 import MateriableBlog from './components/blog';
 import MateriablePortfolio from './components/portfolio';
 import MateriableOther from './components/other';
+import MateriableAbout from './components/about';
 import MateriableContact from './components/contact';
 import MateriableFooter from './components/footer';
 
@@ -26,6 +27,7 @@ class Materiable extends React.Component {
             <MateriableBlog />
             <MateriablePortfolio />
             <MateriableOther />
+						<MateriableAbout />
 						<MateriableContact />
             <MateriableFooter />
         	</Block>
