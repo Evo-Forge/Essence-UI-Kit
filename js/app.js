47f21bae2dc3e889eee071605a777c523ba5b4bf
Radu Farcas <f.radu10@yahoo.com>
2016-03-11 11:34:23 +0200

    add contact component in app.js

--- js/app.js
+++ js/app.js
@@ -26,6 +26,7 @@ class Materiable extends React.Component {
             <MateriableBlog />
             <MateriablePortfolio />
             <MateriableOther />
+						<MateriableContact />
             <MateriableFooter />
         	</Block>
         );
